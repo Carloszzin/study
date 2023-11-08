f50e3912611ff8ed46b87c683b84f8245de7be98
carloszzin <whinchestercarlos@gmail.com>
2023-11-07 23:46:43 -0300

    dia 4 de lógica

---
+++ dia-4/index.js
@@ -0,0 +1,13 @@
+let fome = prompt("Você estar com fome? (sim / não)")
+let dinheiro = prompt("Você tem dinheiro? (sim / não)")
+let restaurante = prompt("O restaurante está aberto? (sim / não)")
+
+
+
+if(fome === "não" || dinheiro == "não"){
+    alert("Hoje a janta será em casa!")
+}else if(dinheiro === "sim" && restaurante === "sim"){
+    alert("Hoje a janta será no seu restaurante favorito!")
+}else{
+    alert("Peça um delivery!")
+} 
