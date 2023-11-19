c920bb3f438e9d137171145e8707adbac1b9e2ff
carloszzin <whinchestercarlos@gmail.com>
2023-11-18 22:21:39 -0300

    praticando Arrays no dia-10

---
+++ dia-10/array.js
@@ -0,0 +1,49 @@
+//Encontrar o número inserido em um indice, e printar isso.
+
+/*
+let list = []
+let indice = []
+let contador = 0
+
+const numeroProcurar = parseInt(prompt("Coloque o número para procurar:"))
+
+for(i = 0; i < 10; i++){
+   const elemento = parseInt(prompt(`O indice do elemento ${i + 1}:`))
+   list[i] = elemento
+}
+
+for(i = 0; i < 10; i++){
+    if(list[i] === numeroProcurar){
+        indice[contador] = i
+        contador++
+    }
+}
+
+console.log(`O número ${numeroProcurar} foi encontrado no indice: ${indice}`)
+*/
+
+
+
+//Array contado e invertido
+
+/*
+let lista = []
+let invertida = []
+let quantidade = parseInt(prompt("Coloque a quantidade de números que deseja inserir: "))
+
+for(i = 0; i < quantidade; i++){
+    let sequencia = parseInt(prompt("Coloque uma contagem "+ (i + 1) ))
+    lista[i] = sequencia
+}
+
+console.log("Array contado: "+lista)
+
+
+let contador = quantidade - 1
+for(i = 0; i < quantidade; i++){
+    invertida[i] = lista[contador]
+    contador--
+}
+
+console.log("Array invertido: "+invertida)
+*/
