e05cb576ac3a2195c29b286a62287172a3f1c039
imcarlosdavi <carlosdv467@gmail.com>
2023-12-22 12:43:35 -0300

    reaproveitado na game-store-simple

--- dia-5/scase.js
+++ dia-5/scase.js
@@ -32,26 +32,58 @@ switch (operador) {
 
 let quantidade
 let valor
+let escolha
 let operador = parseInt(prompt("Bem vindo ao Dav-Games, Quais opcões deseja?"
-+"\n1= jogo de luta; \n2= jogo de card; \n3 = jogo de tiro"))
++"\n1= jogo de Luta; \n2= jogo de Futebol; \n3 = jogo de Tiro"))
+
 
 
 switch (operador) {
     case 1:
-        valor = parseInt(prompt("Digite a quantidade desejada (R$42 por Jogo): "))
-        quantidade = valor * 42;
-        alert("Valor: "+quantidade+" R$ nos jogos de luta")
+        escolha = prompt("Temos 3 opções: \nDragonBall \nTekken7 \nMortal Kombat11")
+        valor = parseInt(prompt("Digite a quantidade desejada (R$58 por Jogo): "))
+        quantidade = valor * 58;
+        if(escolha == "DragonBall"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de luta`
+        }else if(escolha == "Tekken7"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de luta`
+        }else if(escolha == "Mortal Kombat"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de luta`
+        }else{
+            alert("Digite o nome corretamente..")
+        }
         break;
+
     case 2:
-        valor = parseInt(prompt("Digite a quantidade desejada (R$37 por Jogo): "))
-        quantidade = valor * 37;
-        alert("Valor: "+quantidade+" R$ nos jogos de card")
+        escolha = prompt("Temos 3 opções: \nEA Sports \nEfootbal+ \nFIFA 23")
+        valor = parseInt(prompt("Digite a quantidade desejada (R$116 por Jogo): "))
+        quantidade = valor * 116;
+        if(escolha == "EA Sports"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de Futebol`
+        }else if(escolha == "Efootbal+"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de Futebol`
+        }else if(escolha == "FIFA 23"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de Futebol`
+        }else{
+            alert("Digite o nome corretamente..")
+        }
         break;
+
     case 3:
-        valor = parseInt(prompt("Digite a quantidade desejada (R$46 por Jogo): "))
-        quantidade = valor * 46;
-        alert("Valor: "+quantidade+" R$ nos jogos de tiro")
+        escolha = prompt("Temos 3 opções: \nCall of Duty Vanguard \nFar Cry 6 \n Battlefield 5")
+        valor = parseInt(prompt("Digite a quantidade desejada (R$89 por Jogo): "))
+        quantidade = valor * 89;
+        if(escolha == "Call of Duty Vanguard"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de tiro`
+        }else if(escolha == "Far Cry 6"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de tiro`
+        }else if(escolha == "Battlefield 5"){
+            document.body.innerHTML+= `O jogo: ${escolha} tem o Valor: ${quantidade} R$ nos jogos de tiro`
+        }else{
+            alert("Digite o nome corretamente..")
+        }
         break;
+
     default:
         alert("Mais opcões adicionadas em breve...")
         break;
